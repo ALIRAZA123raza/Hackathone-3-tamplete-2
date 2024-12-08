c1c0183fd593acd72cd9ad15995c24f6fbe2dc00
unknown <aa2268964@gmail.com>
2024-12-08 22:39:29 +0500

    Your commit message

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -6,19 +6,18 @@ const TopHeader = () => {
   return (
     <div>
       {/* Header with logo */}
-      <div className='w-full border-b-2 border-gray-300 p-4 relative'>
-        <h1 className='text-center text-2xl mb-2'>Avion</h1>
-        <Search className='absolute left-2 top-2 mt-6' />
-        {/* Shopping Cart and User */}
-        <div className='absolute right-2 top-2 mt-6 flex items-center gap-4'>
+      <div className='w-full border-b-2 border-gray-300 p-4 relative flex items-center justify-between'>
+        <Search className='hidden sm:block absolute left-4 top-1/2 transform -translate-y-1/2' />
+        <h1 className='text-center text-xl sm:text-2xl flex-1'>Avion</h1>
+        <div className='absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center gap-4'>
           <ShoppingCart />
-          <span><User/></span>
+          <User />
         </div>
       </div>
 
       {/* Navigation Links */}
-      <div className='max-w-7xl mx-auto flex justify-center items-center'>
-        <div className='flex gap-6 text-center text-[#726E8D]'>
+      <div className='max-w-7xl mx-auto'>
+        <div className='flex flex-wrap gap-4 justify-center p-4 text-sm sm:text-base text-[#726E8D]'>
           <Link href={"#"}>Plants Pots</Link>
           <Link href={"#"}>Ceramics</Link>
           <Link href={"#"}>Tables</Link>
