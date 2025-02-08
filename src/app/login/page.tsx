c758f618e6882e6ada8b006306f9ac665e35b56f
unknown <aa2268964@gmail.com>
2025-02-08 20:48:04 +0500

    more one  multiples functionalty add my project

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -26,7 +26,7 @@ const LoginPage = () => {
             </button>
           </form>
           <p className="mt-4 text-sm text-gray-600">
-            Don't have an account? <a href="/signup" className="text-blue-600 hover:underline">Sign up</a>
+            Don&apos;t have an account? <a href="/signup" className="text-blue-600 hover:underline">Sign up</a>
           </p>
         </div>
       </div>
